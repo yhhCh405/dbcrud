e618551791943f919dd619965ead1435575e04f8
Ye Htet Hein <yhhCh405@users.noreply.github.com>
2019-08-07 09:30:11 +0630

    Add files via upload

---
+++ var.php
@@ -0,0 +1,8 @@
+<?php
+class globalVariables{
+	protected $db_server 	= "127.0.0.1";
+	protected $db_username	= "root";
+	protected $db_password 	= "";
+	protected $db_name 		= "final_project";
+}
+?>
