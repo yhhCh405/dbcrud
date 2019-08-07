ef475007a0e514b03e08767e084be431e2834e8d
Ye Htet Hein <yhhCh405@users.noreply.github.com>
2019-08-07 14:27:00 +0630

    Update config.php

--- config.php
+++ config.php
@@ -3,6 +3,6 @@ class globalVariables{
 	protected $db_server 	= "127.0.0.1";
 	protected $db_username	= "root";
 	protected $db_password 	= "";
-	protected $db_name 		= "final_project";
+	protected $db_name 	= "test_db";
 }
 ?>
